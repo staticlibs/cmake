47127eceb68414d607ddb77b5a4e50ba76370959
alex <alex@staticlibs.net>
2014-12-30 21:18:16 +0000

    default build types support

--- toolchains/toolchain_x86_64_linux_clang.cmake
+++ toolchains/toolchain_x86_64_linux_clang.cmake
@@ -3,5 +3,8 @@ cmake_minimum_required ( VERSION 2.8 )
 
 set ( CMAKE_SYSTEM_NAME Linux )
 set ( CMAKE_CXX_COMPILER "clang++" )
-set ( CMAKE_CXX_FLAGS "-Os --std=c++11 -Wall -Werror -Wextra" CACHE STRING "compiler flags" )
+set ( CMAKE_CXX_FLAGS "--std=c++11 -Wall -Werror -Wextra" CACHE STRING "compiler flags" )
+set ( CMAKE_CXX_FLAGS_DEBUG "-g -O0" CACHE STRING "compiler debug flags" )
+set ( CMAKE_CXX_FLAGS_RELEASE "-Os" CACHE STRING "compiler release flags" )
+set ( CMAKE_BUILD_TYPE "Debug" CACHE STRING "default build type" )
 
--- toolchains/toolchain_x86_windows_msvc.cmake
+++ toolchains/toolchain_x86_windows_msvc.cmake
@@ -6,5 +6,5 @@ set ( CMAKE_CXX_COMPILER "cl.exe" )
 set ( CMAKE_CXX_FLAGS "/W4 /WX /EHsc" CACHE STRING "compiler flags" )
 set ( CMAKE_CXX_FLAGS_DEBUG "/D_DEBUG /MDd /Zi /Ob0 /Od /RTC1" CACHE STRING "compiler flags debug" )
 set ( CMAKE_CXX_FLAGS_RELEASE "/MT /Os /D NDEBUG" CACHE STRING "compiler flags release" )
-set ( CMAKE_BUILD_TYPE "Debug" )
+set ( CMAKE_BUILD_TYPE "Debug" CACHE STRING "default build type" )
 
