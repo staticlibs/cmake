50faa97162191ec74ef7c17bac7ccecbab320201
alex <alex@staticlibs.net>
2015-01-11 20:23:17 +0000

    test macro update

--- macros/staticlib_enable_testing.cmake
+++ macros/staticlib_enable_testing.cmake
@@ -5,6 +5,7 @@ macro ( staticlib_enable_testing _libs_var_name )
     get_directory_property(${PROJECT_NAME}_has_parent PARENT_DIRECTORY)
     if ( NOT ${PROJECT_NAME}_has_parent AND NOT ${CMAKE_SYSTEM_NAME} MATCHES "Generic" )
         enable_testing ( )
+        include_directories ( ${CMAKE_CURRENT_LIST_DIR}/${STATICLIB_CXX_TEST_DIR} )
         file ( GLOB ${PROJECT_NAME}_TEST_SRC
                 RELATIVE ${CMAKE_CURRENT_LIST_DIR}/${STATICLIB_CXX_TEST_DIR}
                 ${CMAKE_CURRENT_LIST_DIR}/${STATICLIB_CXX_TEST_DIR}/*${STATICLIB_CXX_TEST_POSTFIX} )
