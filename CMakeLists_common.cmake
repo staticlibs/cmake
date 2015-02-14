13c554f2065ccc7f0cdae8a9d4302a5c8c5be02b
alex <alex@staticlibs.net>
2015-02-14 23:44:13 +0000

    macros include fix

--- CMakeLists_common.cmake
+++ CMakeLists_common.cmake
@@ -1,5 +1,5 @@
 
-cmake_minimum_required ( VERSION 2.8 )
+cmake_minimum_required ( VERSION 2.8.3 )
 
 # not a project, should be used with 'include'
 
@@ -8,7 +8,7 @@ OPTION ( CMAKE_INCLUDE_DIRECTORIES_BEFORE "Prefer staticlibs libs over system on
 
 # include all macros
 file ( GLOB_RECURSE STATICLIB_COMMON_MACROS 
-    ${CMAKE_CURRENT_LIST_DIR}/cmake/macros/*.cmake )
+    ${CMAKE_CURRENT_LIST_DIR}/macros/*.cmake )
 foreach ( _commonmacro ${STATICLIB_COMMON_MACROS} )
     include ( ${_commonmacro} )
 endforeach ( )
