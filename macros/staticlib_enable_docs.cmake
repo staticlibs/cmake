f6481122e2ec9b7a91a25d77780f9157cedc31de
alex <alex@staticlibs.net>
2015-02-15 00:48:17 +0000

    doxygen fixes

--- macros/staticlib_enable_docs.cmake
+++ macros/staticlib_enable_docs.cmake
@@ -4,7 +4,7 @@ cmake_minimum_required ( VERSION 2.8 )
 macro ( staticlib_enable_docs )
     option( STATICLIB_GENERATE_DOCS "generate docs" OFF )
     if ( STATICLIB_GENERATE_DOCS ) 
-        configure_file ( ${STATICLIB_CURRENT_LIST_DIR}/cmake/doxygen/Doxyfile 
+        configure_file ( ${CMAKE_CURRENT_LIST_DIR}/cmake/doxygen/Doxyfile 
             ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile )
         execute_process ( COMMAND doxygen ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile )
     endif ( ) 
