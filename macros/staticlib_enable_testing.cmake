ce7b76618fe368a35e44173cc1b076c71864286d
alex <alex@staticlibs.net>
2015-05-20 18:26:21 +0100

    use compile options instead of definitions

--- macros/staticlib_enable_testing.cmake
+++ macros/staticlib_enable_testing.cmake
@@ -18,7 +18,7 @@ cmake_minimum_required ( VERSION 2.8.12 )
 # enabled only for top-level project
 # includes, libs and definitions are passed as variable names (NOT values)
 
-macro ( staticlib_enable_testing _includes_var_name _libs_var_name _defs_var_name )
+macro ( staticlib_enable_testing _includes_var_name _libs_var_name _opts_var_name )
     if ( "${STATICLIB_ROOT_SOURCE_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}" )
         set ( ${PROJECT_NAME}_CXX_EXTENSION cpp CACHE INTERNAL "" )
         set ( ${PROJECT_NAME}_TEST_DIRECTORY test CACHE INTERNAL "" )
@@ -33,7 +33,7 @@ macro ( staticlib_enable_testing _includes_var_name _libs_var_name _defs_var_nam
             add_executable ( ${_testname} ${CMAKE_CURRENT_LIST_DIR}/test/${_testsrc} )
             target_include_directories ( ${_testname} BEFORE PRIVATE ${${_includes_var_name}} )
             target_link_libraries ( ${_testname} ${${_libs_var_name}} )
-            target_compile_definitions ( ${_testname} PRIVATE ${${_defs_var_name}} )
+            target_compile_options ( ${_testname} PRIVATE ${${_opts_var_name}} )
             if ( DEFINED CMAKE_MEMORYCHECK_COMMAND )
                 add_test ( ${_testname} 
                         ${CMAKE_MEMORYCHECK_COMMAND} ${CMAKE_MEMORYCHECK_COMMAND_OPTIONS} 
