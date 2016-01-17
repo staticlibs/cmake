588d22bd75b0d92378ca65852a982de149b3e53d
alex <alex@staticlibs.net>
2016-01-17 10:14:35 +0000

    shortcuts update

--- resources/shortcuts/build-macosx-all.sh
+++ resources/shortcuts/build-macosx-all.sh
@@ -5,10 +5,24 @@ set -x
 cd ..
 cd build
 
+echo --- default
+. creset
+cmake .. -DCMAKE_CXX_FLAGS="--std=c++11"
+make
+
 echo --- macosx_amd64_clang
 . creset
 cmake ../test \
     -DSTATICLIB_TOOLCHAIN=macosx_amd64_clang \
+    -DSTATICLIB_USE_DEPLIBS_CACHE=ON
+make
+make test
+
+echo --- macosx_amd64_clang_RELEASE
+. creset
+cmake ../test \
+    -DSTATICLIB_TOOLCHAIN=macosx_amd64_clang \
+    -DCMAKE_BUILD_TYPE=Release \
     -DSTATICLIB_USE_DEPLIBS_CACHE=ON
 make
 make test
---
+++ resources/shortcuts/build-windows-amd64-all.bat
@@ -0,0 +1,41 @@
+@echo on
+
+echo --- default
+cd ..
+rmdir /s /q build || exit /b
+mkdir build || exit /b
+cd build || exit /b
+cmake .. ^
+    -G "Visual Studio 12 2013 Win64" ^
+    -DSTATICLIB_USE_DEPLIBS_CACHE=ON ^
+    || exit /b
+cmake --build . || exit /b
+
+echo --- windows_amd64_msvc
+cd ..
+rmdir /s /q build || exit /b
+mkdir build || exit /b
+cd build || exit /b
+cmake ../test ^
+    -DSTATICLIB_TOOLCHAIN=windows_amd64_msvc ^
+    -G "Visual Studio 12 2013 Win64" ^
+    -DSTATICLIB_USE_DEPLIBS_CACHE=ON ^
+    || exit /b
+cmake --build . || exit /b
+rem ctest || exit /b
+
+echo --- windows_amd64_msvc_RELEASE
+cd ..
+rmdir /s /q build || exit /b
+mkdir build || exit /b
+cd build || exit /b
+cmake ../test ^
+    -DSTATICLIB_TOOLCHAIN=windows_amd64_msvc ^
+    -G "Visual Studio 12 2013 Win64" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DSTATICLIB_USE_DEPLIBS_CACHE=ON ^
+     || exit /b
+cmake --build . --config Release || exit /b
+ctest || exit /b
+
+echo BUILD SUCCESSFUL
